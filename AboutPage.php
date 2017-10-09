30d534bf6b817e836f9db8213281c92c2e62be32
aquiles <aquiles_gual1@hotmail.com>
2017-10-09 13:51:13 +0200

    Added AboutPage

---
+++ AboutPage.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aquil
+ * Date: 10/9/2017
+ * Time: 1:30 PM
+ */
+
+include 'index.view.php';
+?>
+<div class="container-fluid text-center bodyunder">
+    <div class="row content">
+        <div class="col-sm-2 sidenav">
+            <p><a href=""></a></p>
+        </div>
+        <div class="col-sm-8 text-left">
+            <p>
+            <h1>Dol op eten</h1>
+
+            <h3>De liefde voor lekker eten zit in ons DNA.</h3>
+
+            Bij Eurest zijn maaltijden veel meer dan een hapje in de pauze. Onze verse ingrediënten worden bereid tot
+            heerlijke en smakelijke gerechten. Elke dag zetten onze teams zich daar met hart en ziel voor in.
+            We houden van lekker eten en gaan daarbij voor topkwaliteit. Maar hoe goed onze dienstverlening ook is, er
+            is altijd ruimte voor verbetering. Daarom houden we voortdurend alles tegen het licht, van de versheid,
+            voedingswaarde en kennis van onze producten tot de informatievoorziening, service, kostenbeheersing en consumentenkennis.
+            </p>
+        </div>
+    </div>
+</div>
